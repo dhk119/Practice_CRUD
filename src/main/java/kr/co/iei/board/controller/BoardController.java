5de6db77369adb9677b790b00f29200c4932cbfd
dhk119 <daehean95@gmail.com>
2024-09-09 11:46:32 +0900

    게시판 화면구현

--- src/main/java/kr/co/iei/board/controller/BoardController.java
+++ src/main/java/kr/co/iei/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package kr.co.iei.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.board.model.service.BoardService;
@@ -11,4 +12,19 @@ import kr.co.iei.board.model.service.BoardService;
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
+	
+	@GetMapping(value="/boardList")
+	public String boardList() {
+		return "/board/boardList";
+	}//게시판
+	
+	@GetMapping(value="/view")
+	public String boardView() {
+		return "/board/view";
+	}//게시판 상세보기
+	
+	@GetMapping(value="/boardFrm")
+	public String boardFrm() {
+		return "/board/boardFrm";
+	}//글쓰기
 }
